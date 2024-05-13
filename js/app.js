e35f29e61f9f9dec86551063677e90c188ab82f2
lizreznik <525ereznik@frhsd.com>
2024-05-13 10:11:49 -0400

    addToDo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+
+const dateElement = document.getElementById("date");
+
+
+const list = document.getElementById("list");
+
+
+const input = documnt.getElementById("input"); 
+
+//insertAdjacentHTML()
+const element = document.getElementById("element"); 
+// syntax: 
+
+
+// add a to-do
+function addToDo (toDo){ 
+const text = `<li class="item"> 
+<i class="co fa fa-circle-thin" job="complete"></i>
+<p class="text"> Drink Coffee </p>
+<i class="de fa fa-trash-o" job="delete"> </i>
+</li>`
+const position = "beforeend"; 
+list.insertAdjacentHTML(postion, text); 
+}
+addToDo("Drink Coffee"); 
