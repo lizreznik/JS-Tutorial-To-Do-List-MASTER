c15cd3f1bf6886d012af735664010c0a1d8063c8
lizreznik <525ereznik@frhsd.com>
2024-05-13 10:15:48 -0400

    add to do JS

--- js/app.js
+++ js/app.js
@@ -7,21 +7,32 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 
 
-const input = documnt.getElementById("input"); 
+const input = documnt.getElementById("input");
+
 
 //insertAdjacentHTML()
-const element = document.getElementById("element"); 
+const element = document.getElementById("element");
 // syntax: 
 
 
 // add a to-do
-function addToDo (toDo){ 
-const text = `<li class="item"> 
+function addToDo(toDo) {
+    const text = `<li class="item"> 
 <i class="co fa fa-circle-thin" job="complete"></i>
 <p class="text"> Drink Coffee </p>
 <i class="de fa fa-trash-o" job="delete"> </i>
 </li>`
-const position = "beforeend"; 
-list.insertAdjacentHTML(postion, text); 
+    const position = "beforeend";
+    list.insertAdjacentHTML(postion, text);
 }
-addToDo("Drink Coffee"); 
+addToDo("Drink Coffee");
+
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
+        const toDo = input.value;
+        if (toDo) {
+            addToDo(toDo);
+        }
+        input.value = "";
+    }
+}); 
